393b3d7bc64497558d0754a40bb59124a94975e7
Chloe <chloe@Chloes-MacBook-Air.local>
2020-02-29 18:24:18 +0100

    ShoppingList

---
+++ client/src/store.js
@@ -0,0 +1,14 @@
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducer';
+
+const initialtate = {};
+
+const middleware = [thunk];
+
+const store = createStore(rootReducer, initialtate, compose(
+  applyMiddleware(...middleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+))
+
+export default store
