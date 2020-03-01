4b27eaa47862d239f29491c3a17cb45c6f8bcbe7
Chloe <chloe@Chloes-MacBook-Air.local>
2020-03-01 17:55:58 +0100

    version 1.0.0

--- server.js
+++ server.js
@@ -1,31 +1,42 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-const items = require('./routes/api/items')
+const items = require("./routes/api/items");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
 // BodyParser middleware
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // DB config
 const db = process.env.MONGODB_URI;
 
 //Connect to mongo
 mongoose
-  .connect(db,{
+  .connect(db, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(()=> console.log('MongoDB connected....'))
-  .catch((err)=> console.log(err))
+  .then(() => console.log("MongoDB connected...."))
+  .catch(err => console.log(err));
 
 //Use routes
-app.use('/api/items', items)
+app.use("/api/items", items);
+
+//Serve puplic assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>console.log(`Server started on port ${port} `));
+app.listen(port, () => console.log(`Server started on port ${port} `));
